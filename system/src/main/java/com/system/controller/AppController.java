613787a5780e1e2263f275345beec8df60b0eba0
Udula Kesara Muniweera <153984452+ukmuniweera@users.noreply.github.com>
2025-05-28 16:05:18 +0530

    Update AppController.java

--- system/src/main/java/com/system/controller/AppController.java
+++ system/src/main/java/com/system/controller/AppController.java
@@ -1,29 +1,62 @@
 package com.system.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.system.model.Course;
-import com.system.model.Student;
+import java.util.*;
+import org.springframework.web.bind.annotation.*;
+import com.system.model.*;
 
 @RestController
-@RequestMapping("/app")
+@RequestMapping("/")
 public class AppController {
-	Student Bob = new Student("No.1", "Bob Marley", 23, "IT", 3.2);
-	Student James = new Student("No.2", "James Bond", 24, "IT", 2.2);
-	Student Donald = new Student("No.3", "Donald Trump", 25, "IT", 1.2);
+	Student Bob = new Student("1", "Bob Marley", 23, "IT", 3.2);
+	Student James = new Student("2", "James Bond", 24, "IT", 2.2);
+	Student Donald = new Student("3", "Donald Trump", 25, "IT", 1.2);
 
 	Course IT = new Course("IT", "Information Technology");
 	Course AMC = new Course("AMC", "Advanced Mathematics Computing");
 
-	private static List<Student> students = new ArrayList<Student>();
 	private Map<String, Student> mstudents = new HashMap<String, Student>();
-
-	private static List<Course> courses = new ArrayList<Course>();
 	private Map<String, Course> mcourses = new HashMap<String, Course>();
+
+	private AppController() {
+		mstudents.put(Bob.getRegNo(), Bob);
+		mstudents.put(James.getRegNo(), James);
+		mstudents.put(Donald.getRegNo(), Donald);
+
+		mcourses.put(IT.getCid(), IT);
+		mcourses.put(AMC.getCid(), AMC);
+	}
+
+	@GetMapping("students")
+	public Map<String, Student> getStudents() {
+		return mstudents;
+	}
+
+	@GetMapping("students/{id}")
+	public Student getStudent(@PathVariable("id") String RegNo) {
+		return mstudents.get(RegNo);
+	}
+
+	@PostMapping("students")
+	public String addStudent(@RequestBody Student student) {
+		mstudents.put(student.getRegNo(), student);
+		return "Student successfully added";
+	}
+
+	@DeleteMapping("students/{id}")
+	public String deleteStudent(@PathVariable("id") String RegNo) {
+		if (mstudents.get(RegNo) != null) {
+			mstudents.remove(RegNo);
+			return "Student successfully deleted";
+		}
+		return "Student not found";
+	}
+
+	@PutMapping("students/{id}")
+	public String updateStudent(@PathVariable("id") String RegNo, @RequestBody Student student) {
+		if (mstudents.get(RegNo) != null) {
+			mstudents.put(student.getRegNo(), student);
+			return "Student successfully updated";
+		}
+		return "Student not found";
+	}
 }
